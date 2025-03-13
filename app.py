d674d92cb12cb75e770973b01aa5a9070af368f9
Lucifer <itzmegagan2@gmail.com>
2025-03-14 00:02:50 +0530

    completed some of the pages

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, redirect, url_for, request, flash, ses
 from flask_mysqldb import MySQL
 import os, bcrypt
 from dotenv import load_dotenv
+from werkzeug.utils import secure_filename
+
 
 load_dotenv()
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {"png","jpg","jpeg","gif"}
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
@@ -12,7 +17,25 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 mysql = MySQL(app)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+def handle_recipe_results(db_results):
+    recipe_list = []
+    for row in db_results:
+        recipe_dict = {
+            "recipe_id":row[0],
+            "title":row[1],
+            "description":row[2],
+            "image_path":row[3]
+        }
+        recipe_list.append(recipe_dict)
+        
+    return recipe_list
+
 
 @app.route('/', methods=["GET", "POST"])
 def login():
@@ -20,11 +43,12 @@ def login():
         email = request.form["Email"]
         password = request.form["Password"].encode('utf-8')
         cursor = mysql.connection.cursor()
-        cursor.execute("select email, password from Users where email=%s ", (email,))
+        cursor.execute("select user_id,email, password from Users where email=%s ", (email,))
         result = cursor.fetchone()
         if result:
-            db_email, db_password = result
+            user_id, db_email, db_password = result
             if bcrypt.checkpw(password, db_password):
+                session["user_id"] = user_id
                 session["email"] = email
                 session["password"] = password
                 flash("Login success", "success")
@@ -49,6 +73,7 @@ def register():
         cursor.execute("select username,email from Users where username=%s and email=%s", (user_name,email))
         result = cursor.fetchone()
         if result:
+            print(result)
             flash("Username taken", "danger")
             return redirect(url_for('register'))
         hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
@@ -58,7 +83,7 @@ def register():
             flash("Registration successful", "success")
             return redirect(url_for('login'))
         except Exception as e:
-            flash(f"Registration failed: {e}", "danger")
+            flash(f"Registration failed: ", "danger")
             mysql.connection.rollback()
         finally:
             cursor.close()
@@ -68,5 +93,91 @@ def register():
 def home():
     return render_template("home.html")
 
+@app.route('/AddRecipe',methods=["GET","POST"])
+def add_recipe():
+    if request.method == "POST":
+        file = request.files['image']
+        if file.filename == '':
+           return "No selected file"
+        if allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.static_folder, 'uploads', filename))
+            file_save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename).replace('\\', '/')
+            category = request.form["category"]
+            title = request.form["title"]
+            description = request.form["description"]
+            instructions = request.form["instructions"]
+            ingredients = request.form['ingredients']
+            
+            try:
+                cursor = mysql.connection.cursor()
+                cursor.execute("select category_id from Category where category_name=%s",(category,))
+                result = cursor.fetchone()
+                if result:
+                    category_id = result[0]
+                    user_id = session.get("user_id")
+                    cursor.execute("insert into Recipes (user_id, title, description, ingredients, instructions, category_id, image_path) values(%s,%s,%s,%s,%s,%s,%s)",(user_id,title,description,ingredients,instructions,category_id,file_save_path))
+                    mysql.connection.commit()
+                    flash("Recipe uploaded successfully","success")
+                    return redirect(url_for("add_recipe"))
+                else:
+                    flash("category required")
+                    return redirect(url_for('add_recipe'))
+            except Exception as e:
+                flash("Some error occured try again later", "danger")
+                return redirect(url_for('add_recipe')) 
+        else:
+           flash("allowed images are ->png,jpg,jpeg,gif")
+           return render_template("addARecipe.html")
+    return render_template("addARecipe.html")
+    
+@app.route('/recipes')
+def Recipes():
+    try :
+        print("working")
+        cursor = mysql.connection.cursor()
+        cursor.execute("select Recipe_id, title, description,image_path from Recipes")
+        temp = cursor.fetchall()
+        if not temp:
+            raise Exception("No data found")
+        recipes_list = handle_recipe_results(temp)
+        print(recipes_list)
+        return render_template("Recipes.html",recipes = recipes_list)
+
+    except Exception as e:
+        print(f"exception :{e}")
+        flash("No recipes found","danger")
+        return render_template("Recipes.html")
+
+@app.route('/read-full-recipe/<int:recipe_id>')
+def complete_recipe(recipe_id):
+    if not recipe_id:
+        flash(f"Some error occured please try again later","danger")
+        return render_template("CompleteRecipe.html")
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("select image_path, description, ingredients,instructions from Recipes where Recipe_id = %s",(recipe_id,))
+        db_recipe_data = cursor.fetchone()
+        if not db_recipe_data:
+            raise Exception("Some error occured please try again later")
+        recipe_data = []
+        print(db_recipe_data)
+        recipe_dict = {
+            "recipe_id":recipe_id,
+            "image_path":db_recipe_data[0],
+            "description":db_recipe_data[1],
+            "ingredients":db_recipe_data[2],
+            "instruction":db_recipe_data[3]
+        }
+        recipe_data.append(recipe_dict)
+        return render_template("CompleteRecipe.html",recipes = recipe_data)
+    except Exception as e:
+        flash(f"{e}","danger")
+        render_template("CompleteRecipe.html")
+    return render_template("CompleteRecipe.html")
+
+@app.route('/your-recipes')
+def your_recipes():
+    return render_template("your_recipes.html")
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ temp.py
@@ -0,0 +1,8 @@
+import os
+
+folder = "uploads"
+filename = "my_file.txt"
+
+# Using os.path.join()
+full_path = os.path.join(folder, filename)
+print(full_path)
